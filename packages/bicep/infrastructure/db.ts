0e270caed509d3eff22983fcf03d05df81052277
raphrueda <raphrueda@gmail.com>
2020-08-25 13:53:21 +1000

    Basic db script

---
+++ packages/bicep/infrastructure/db.ts
@@ -0,0 +1,31 @@
+import { config } from 'dotenv';
+import { Pool } from 'pg';
+
+config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+});
+
+(async () => {
+    console.log('Creating tables...');
+
+    //#region Users
+    console.log('Creating users table...');
+    const test = await pool.query(
+        `
+    CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(30) UNIQUE,
+        email VARCHAR(255) UNIQUE,
+        pw_hash TEXT
+    );
+    `,
+    );
+    console.log(test);
+    //#endregion
+})();
