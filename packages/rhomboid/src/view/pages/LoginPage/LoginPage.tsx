2abd22fc09e0d6de1118261e2a715c520b6f36e2
Raphael.Rueda <raphael.rueda@iress.com>
2020-08-26 11:30:10 +1000

    Remove sass from pages and use material style hook

--- packages/rhomboid/src/view/pages/LoginPage/LoginPage.tsx
+++ packages/rhomboid/src/view/pages/LoginPage/LoginPage.tsx
@@ -1,43 +1,56 @@
-import { Button, Link, Paper, TextField, Typography } from '@material-ui/core';
+import { Button, Link, makeStyles, Paper, TextField, Typography } from '@material-ui/core';
 import * as React from 'react';
 
 import { Inline, InlineSpacing, Stack, StackSpacing } from '../../components/layout';
 
-const CLASS_NAME = 'login-page';
-const LOGIN_CLASS_NAME = `${CLASS_NAME}__login-section`;
+const useStyles = makeStyles((theme) => ({
+    loginPage: {
+        display: 'flex',
+        height: '100vh',
+        background: '#eee',
+    },
+    loginPaper: {
+        width: '350px',
+        margin: 'auto',
+        padding: '1rem',
+    },
+}));
 
-export const LoginPage: React.FunctionComponent = () => (
-    <div className={CLASS_NAME}>
-        <Paper className={LOGIN_CLASS_NAME}>
-            <Stack spacing={StackSpacing.Large}>
-                <Typography variant="h5">Hand stand</Typography>
-                <TextField
-                    label="Username/email"
-                    size="small"
-                    variant="outlined"
-                    fullWidth
-                    required
-                />
-                <TextField
-                    label="Password"
-                    type="password"
-                    size="small"
-                    variant="outlined"
-                    fullWidth
-                    required
-                />
-                <Inline spacing={InlineSpacing.Medium} center pullRight>
-                    <Typography>
-                        <Link href="#/">Forgot password?</Link>
-                    </Typography>
-                    <Button variant="contained" color="default" size="medium">
-                        Signup
-                    </Button>
-                    <Button variant="contained" color="primary" size="medium">
-                        Login
-                    </Button>
-                </Inline>
-            </Stack>
-        </Paper>
-    </div>
-);
+export const LoginPage: React.FunctionComponent = () => {
+    const styles = useStyles();
+    return (
+        <div className={styles.loginPage}>
+            <Paper className={styles.loginPaper}>
+                <Stack spacing={StackSpacing.Large}>
+                    <Typography variant="h5">Hand stand</Typography>
+                    <TextField
+                        label="Username/email"
+                        size="small"
+                        variant="outlined"
+                        fullWidth
+                        required
+                    />
+                    <TextField
+                        label="Password"
+                        type="password"
+                        size="small"
+                        variant="outlined"
+                        fullWidth
+                        required
+                    />
+                    <Inline spacing={InlineSpacing.Medium} center pullRight>
+                        <Typography>
+                            <Link href="#/">Forgot password?</Link>
+                        </Typography>
+                        <Button variant="contained" color="default" size="medium">
+                            Signup
+                        </Button>
+                        <Button variant="contained" color="primary" size="medium">
+                            Login
+                        </Button>
+                    </Inline>
+                </Stack>
+            </Paper>
+        </div>
+    );
+};
