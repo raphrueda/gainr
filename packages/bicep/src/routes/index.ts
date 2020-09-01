fc2dcb16e79c722f2ff02a65ed58376e09edd66b
Raphael.Rueda <raphael.rueda@iress.com>
2020-09-01 22:51:55 +1000

    Add protected test route

--- packages/bicep/src/routes/index.ts
+++ packages/bicep/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { verify } from 'jsonwebtoken';
 
 import { auth } from './auth/auth.route';
 
@@ -6,6 +7,32 @@ const router = Router();
 
 // Test route
 router.get('/', (_req, res) => res.send('Yay'));
+// Protected
+router.get(
+    '/supersecret',
+    (req, res, next) => {
+        const authToken = req.headers['authorization'];
+
+        if (!authToken) {
+            throw new Error('Missing authorization token');
+        }
+
+        // TODO: Duplicated route logic. Abstract into utils
+        if (!process.env?.ACCESS_TOKEN_SECRET) {
+            console.error('Missing access secret.');
+            throw new Error('Something went wrong.');
+        }
+
+        try {
+            const token = authToken.split(' ')[1];
+            verify(token, process.env.ACCESS_TOKEN_SECRET);
+        } catch (error) {
+            throw new Error('Bad token.');
+        }
+        return next();
+    },
+    (req, res) => res.send("You've accessed the super secret route"),
+);
 
 router.use('/auth', auth);
 
