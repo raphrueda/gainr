44c9ac1a56a38cb975d8616096f0df05e0ec962e
Raphael.Rueda <raphael.rueda@iress.com>
2020-09-01 22:10:51 +1000

    Remove unnecessary log

--- packages/bicep/src/routes/route.utils.ts
+++ packages/bicep/src/routes/route.utils.ts
@@ -4,7 +4,6 @@ import { ObjectSchema } from 'joi';
 import { SchemaValidationError } from './route.errors';
 
 export const validate = (schema: ObjectSchema): RequestHandler => (req, res, next) => {
-    console.log(req.body);
     const { error } = schema.validate(req.body);
     if (error) {
         next(new SchemaValidationError(error));
