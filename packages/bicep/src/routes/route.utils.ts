4e5110ecfa2a2f8b849aab68d8d801ca0380d3f7
Raphael.Rueda <raphael.rueda@iress.com>
2020-11-19 00:15:41 +1100

    Abstract auth check into request middleware

--- packages/bicep/src/routes/route.utils.ts
+++ packages/bicep/src/routes/route.utils.ts
@@ -1,7 +1,10 @@
 import { RequestHandler } from 'express';
 import { ObjectSchema } from 'joi';
 
+import { getEnvVar } from '@utils/common';
+
 import { SchemaValidationError } from './route.errors';
+import { verify } from 'jsonwebtoken';
 
 export const validate = (schema: ObjectSchema): RequestHandler => (req, res, next) => {
     const { error } = schema.validate(req.body);
@@ -9,4 +12,20 @@ export const validate = (schema: ObjectSchema): RequestHandler => (req, res, nex
         next(new SchemaValidationError(error));
     }
     next();
+};
+
+export const requireAuth: RequestHandler = (req, res, next) => {
+    const authToken = req.headers['authorization'];
+    if (!authToken) {
+        return next(new Error('Missing authorization token'));
+    }
+
+    try {
+        const token = authToken.split(' ')[0];
+        const payload = verify(token, getEnvVar('ACCESS_TOKEN_SECRET')) as Record<string, any>;
+        res.locals.userId = payload.userId;
+    } catch (error) {
+        return next(new Error('Bad token.'));
+    }
+    return next();
 };
